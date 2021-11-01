2920982432813655455660df7a22d88d28830afa
Laviniu <83814772+Oct88@users.noreply.github.com>
2021-11-01 16:54:17 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,37 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get(
+    (req, res, next) => {
+    console.log('input required: ', req.query.input);
+    let num = convertHandler.getNum(req.query.input);
+    let unit = convertHandler.getUnit(req.query.input);
+    console.log(`Num is ${num} and unit is ${unit}.`);
+    next();
+  }, 
+  (req, res) => {
+    let num = Number(convertHandler.getNum(req.query.input));
+    let unit = convertHandler.getUnit(req.query.input);
+     if (num && unit) {
+      let retNum = Number(convertHandler.convert(num, unit));
+      let retUnit = convertHandler.getReturnUnit(unit);
+      let str = convertHandler.getString(num, unit, retNum, retUnit);
+      //console.log(typeof str, str);
+      res.json({
+        initNum: num,
+        initUnit: unit,
+        returnNum: retNum,
+        returnUnit: retUnit,
+        string: str
+      });
+    } else if (!num && !unit) {
+      res.json("invalid number and unit");
+    } else if (!num) {
+      res.json('invalid number');
+    } else {
+      res.json('invalid unit');
+    }
+
+  });
+
 };
